c723ee5dbd587f6b059c2dcd909fb6c81a3947ed
dhanasekhar12345 <103568863+dhanasekhar12345@users.noreply.github.com>
2023-05-26 11:00:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Employees from './components/Employees';
+import {BrowserRouter as BrowserRouter,Route, Routes}  from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Add from './components/Add';
+import Edit from './components/Edit';
+import Home from './components/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <BrowserRouter>
+    <div>
+        <div className="header">
+            <div className='dhana'>
+                <NavLink className={({isActive}) => isActive? 'active':''} to="/">Home</NavLink>
+            </div>
+            &nbsp;
+            <div className='sekhar'>
+                <NavLink className={({isActive}) => isActive? 'active':''} to="/Employees">Employees</NavLink>
+            </div>
+            &nbsp;
+            <div className='reddy'>
+                <NavLink className={({isActive}) => isActive? 'active':''} to="/create">Add</NavLink>
+            </div>
+        </div>
+        <div className="content">
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/Employees' element={<Employees/>}/>
+                <Route path='/create' element={<Add/>}/>
+                <Route path='/edit' element={<Edit/>}/>
+            </Routes>
+        </div>
     </div>
-  );
+</BrowserRouter>
+);
+
 }
 
 export default App;
---
+++ src/components/Employee.js
@@ -0,0 +1,28 @@
+const Employee=[
+    {
+       id:"10001",
+       Name:"Bramha",
+       Sex:"M",
+       DOB:"20-5-1999",
+       Salary:"500000",
+       Department:"Sales"  
+    },
+    {
+        id:"10002",
+        Name:"Nandha",
+        Sex:"M",
+        DOB:"1-06-1996",
+        Salary:"600000",
+        Department:"Accounts"  
+     },
+     {
+        id:"10003",
+        Name:"Kala",
+        Sex:"F",
+        DOB:"11-11-1995",
+        Salary:"800000",
+        Department:"HR"  
+     }
+]
+
+export default Employee;
